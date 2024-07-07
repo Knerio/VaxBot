1ecf21feb35c76a6ff2ac556f4851a9e868d6cae
Dario <derioreal63@gmail.com>
2024-07-07 17:52:55 +0200

    fix design

--- src/main/java/de/derioo/module/predefined/join/JoinModule.java
+++ src/main/java/de/derioo/module/predefined/join/JoinModule.java
@@ -8,6 +8,7 @@ import de.derioo.module.Module;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 
+import java.awt.*;
 import java.util.List;
 
 public class JoinModule extends Module {
@@ -31,6 +32,8 @@ public class JoinModule extends Module {
                                 "\n" +
                                 "\n" +
                                 "\n" + "Unser Host Partner »\n" + "https://discord.gg/tube-hosting -> :TubehostingVarilx:")
+                        .setThumbnail("https://cdn.discordapp.com/attachments/1055223755909111808/1160508092757319740/Unbedasasdasdasddsasdasdadsdadasadnannt.png?ex=668bb062&is=668a5ee2&hm=837e1757017c78cf2a1aac5cd95a61cc5a84f6e01f27853f0726941361dd3683&")
+                        .setColor(Color.GREEN)
                         .setImage("https://cdn.discordapp.com/attachments/1055223755909111808/1160507955507101736/Varilx_Tube-hosting_version.png?ex=668bb041&is=668a5ec1&hm=f82784ef44a4d5f7eff42cc85e8cc032b1dda100781be165a604edaf6e78f858&")
                 .build()).queue();
         List<Role> roleObjects = configData.getRoleObjects(Config.Id.Role.PLAYER_ROLE, event.getGuild());
