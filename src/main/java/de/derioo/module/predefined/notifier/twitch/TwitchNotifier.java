033d0dd444c4309f4d3ffa19dc902f7ccdc6a8ab
Dario P <derioreal63@gmail.com>
2024-10-27 18:41:38 +0100

    implement cache to eliminate duplicate notifications

--- src/main/java/de/derioo/module/predefined/notifier/twitch/TwitchNotifier.java
+++ src/main/java/de/derioo/module/predefined/notifier/twitch/TwitchNotifier.java
@@ -14,7 +14,10 @@ import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
 
 import java.awt.*;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class TwitchNotifier {
@@ -22,6 +25,8 @@ public class TwitchNotifier {
     private final NotifierModule module;
     private final DiscordBot bot;
     private final TwitchClient client;
+
+    private final Map<String, Long> notifyCache = new HashMap<>();
 
     public TwitchNotifier(NotifierModule module, DiscordBot bot) {
         this.module = module;
@@ -44,16 +49,24 @@ public class TwitchNotifier {
     private void onGoLive(ChannelGoLiveEvent event) {
         System.out.println(event.getChannel().getName() + " is now live");
         for (Guild guild : this.bot.getJda().getGuilds()) {
+            System.out.println("Trying to notifiy " + guild.getName());
             ConfigData config = bot.get(guild);
             List<String> data = config.getData(Config.Id.Data.TWITCH_NOTIFIER.name(), List.class);
             if (!data.contains(event.getChannel().getName())) continue;
             TextChannel channel = guild.getTextChannelById(config.getChannels().get(Config.Id.Channel.TWITCH_NOTIFY_CHANNEL.name()));
             List<Role> roles = config.getRoleObjects(Config.Id.Role.TWITCH_PING_ROLES, guild);
+            System.out.println("Notifying " + guild.getName());
             notify(event.getStream(), channel, roles);
         }
     }
 
     private void notify(Stream stream, TextChannel channel, List<Role> roles) {
+        notifyCache.putIfAbsent(stream.getUserName(), System.currentTimeMillis());
+        if (System.currentTimeMillis() - notifyCache.get(stream.getUserName()) < TimeUnit.SECONDS.toMillis(120)) {
+            return;
+        }
+        notifyCache.put(stream.getUserName(), System.currentTimeMillis());
+
         String imageUrl = client.getHelix().getUsers(null, null, List.of(stream.getUserName())).execute().getUsers().getFirst().getProfileImageUrl();
         String streamUrl = "https://twitch.tv/" + stream.getUserName();
         channel.sendMessage("> **" + stream.getUserName() + "** ist live auf Twitch gegangen, schaut vorbei! " +
